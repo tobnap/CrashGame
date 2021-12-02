67df67bcf8d2659cffa504c208dc2c412e726409
11851 <11851@2023NapolitanoT.local>
2021-12-01 23:51:09 -0500

    added crash function

--- js/index.js
+++ js/index.js
@@ -1,18 +1,12 @@
+var graphInterval;
+var count = 0;
+
 var xValues = [];
 var yValues = [];
 generateData("x", 1, 10, 1);
 
 var chart = new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-        fill: false,
-        pointRadius: 2,
-        borderColor: "rgba(0,0,255,0.5)",
-        data: yValues
-    }]
-  },    
+  type: "line",   
   options: {
     scales: {
         yAxes: [{
@@ -45,13 +39,13 @@ function generateData(value, i1, i2, step = 1) {
   }
 }
 
-var graphInterval;
-
 function playCrash() {
     graphInterval = setInterval(updateGraph, 100);
+    setTimeout(function() {
+        clearInterval(graphInterval);
+    }, (Math.log(100/(100/Math.random()*99.9+0.1))/Math.log(1.1))/0.6*1000);
+    count = 0;
 }
-
-var count = 0;
 
 function updateGraph() {
     xValues = [];
@@ -61,18 +55,18 @@ function updateGraph() {
     chart.data.labels = xValues;
     chart.data.datasets = [{
         fill: false,
-        pointRadius: 2,
+        pointRadius: 0,
         borderColor: "rgba(0,0,255,0.5)",
         data: yValues
     }];
 
-    chart.options.scales.yAxes[0].ticks.max = yValues.at(-1);
+    if (yValues.at(-1) < 2) {
+        chart.options.scales.yAxes[0].ticks.max = 2;
+    } else {
+        chart.options.scales.yAxes[0].ticks.max = yValues.at(-1);
+    }
 
     chart.update();
 
     count++;
-}
-
-function stopGraph() {
-    clearInterval(graphInterval);
 }
